33a43de4d0d6de621df289fdefd562c26b85d59a
Lars Nielsen <lnc13.lars@gmail.com>
2019-04-03 12:00:11 +0200

    added ability to write empty file

---
+++ src/tartarus/files.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+namespace tartarus
+{
+namespace files
+{
+    /// Creates and empty file at path. Remember to include name in path
+    /// @param path is a string that create gives the path to where the file should be created 
+    void create_file(std::string path)
+    {
+	std::ofstream outfile("./test.txt");
+	outfile.close();         
+    }
+}
+}
