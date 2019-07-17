c920411e4e19f3c39695d42900e93fdbe091727c
Marton Sipos <sipmarton@gmail.com>
2019-07-17 12:19:48 +0200

    Added JSON reader/writer tests along with test data.

--- benchmark/measure_read_write/measure_read_write.cpp
+++ benchmark/measure_read_write/measure_read_write.cpp
@@ -5,7 +5,13 @@
 #include <vector>
 #include <chrono>
 #include <iostream>
+#include <fstream>
 
+std::ifstream::pos_type filesize(const char* filename)
+{
+    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+    return in.tellg(); 
+}
 
 void measure_vector_writer_reader()
 {
@@ -35,11 +41,69 @@ void measure_vector_writer_reader()
 
     //Delete file
     std::remove(path.c_str());
+}
+
+
+void measure_json_writer_reader()
+{
+    int data_size = 0;
+    int iterations = 100000; //iterations must be a multiple of num_files
+    int num_files = 10;
     
+    //Put together the paths and measure total bytes
+    std::vector<std::string> reader_paths;
+    std::vector<std::string> writer_paths;
+    std::vector<nlohmann::json> objects(num_files);
+    for(int i=0; i < num_files; i++)
+    {
+	std::string path = "benchmark/measure_read_write/json_test_data/"
+	    + std::to_string(i) + ".json";
+	reader_paths.push_back(path);
+	data_size += filesize(path.c_str());
+	
+	path = "benchmark/measure_read_write/json_test_data/"
+	    + std::to_string(i) + ".json.out";
+	writer_paths.push_back(path);
+    }
+
+    data_size *= (iterations/num_files);
+    
+    //Reader benchmark
+    auto t1 = std::chrono::high_resolution_clock::now();
+    for(int i=0; i < iterations/num_files; i++){
+	objects[i % num_files] = tartarus::readers::json_reader(reader_paths[i % num_files]);
+    }
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto res_read = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+
+    
+    //Writer benchmark
+    t1 = std::chrono::high_resolution_clock::now();
+    for(int i=0; i < iterations/num_files; i++){
+	tartarus::writers::json_writer(reader_paths[i % num_files], objects[i % num_files]);
+    }
+    t2 = std::chrono::high_resolution_clock::now();
+    auto res_write = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+
+    std::cout<<"JSON results based on " << iterations/num_files << " iterations over "
+	     << num_files << " files of average size: " << data_size / iterations
+	     << " bytes, for a total of " << data_size << " bytes." << std::endl;
+    
+    std::cout<<"JSON writer throughput " << data_size / double(res_write.count()) << "MB/s" << std::endl;
+
+    std::cout<<"JSON reader throughput " << data_size / double(res_read.count()) << "MB/s" << std::endl;
+
+    //Delete files
+    for(int i=0; i < num_files; i++)
+    {
+	std::remove(writer_paths[i].c_str());
+    }
 }
 
 int main(void)
 {
+    std::cout << "Note: benchmark must be run from project's root directory!" <<std::endl;
     measure_vector_writer_reader();
+    measure_json_writer_reader();
     return 0;
 }
